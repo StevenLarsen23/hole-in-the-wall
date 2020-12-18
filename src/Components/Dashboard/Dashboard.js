51aa019a33129f6699db2a189af6ef4a7305cf93
Steven Larsen <slarsen0823@gmail.com>
2020-12-18 12:30:42 -0700

    commit

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Dashboard.css'
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    return (
-        <div className='dashboard'>Dashboard Component</div>
-    )
-}
+  return (
+    <div className="dashboard">
+      <div className="dash-header">
+        <h3 className="welcome">Welcome to the state of:</h3>
+        <h1 className="state">Your State Here</h1>
+        <h4>Check out some of these great places to eat</h4>
+      </div>
+      <div>
+          <ul>
+              <li>
+                  <img alt=''/>
+              </li>
+          </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
