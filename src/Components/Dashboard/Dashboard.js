68f8b92e9681a9edf70436ef25c19af67df18a9a
Steven Larsen <slarsen0823@gmail.com>
2020-12-18 14:19:01 -0700

    built dashboard with place holder data

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -9,11 +9,22 @@ const Dashboard = () => {
         <h4>Check out some of these great places to eat</h4>
       </div>
       <div>
-          <ul>
-              <li>
-                  <img alt=''/>
-              </li>
-          </ul>
+        <ul className="list">
+          <li className='list-items'>
+            <img
+              alt="R Pizza Place"
+              src="https://theswellutah.com/wp-content/uploads/2019/04/R_Pizza_Place_03-1920x1280.jpg"
+            />
+            <p>R Pizza Place</p>
+          </li>
+          <li className='list-items'>
+            <img
+              alt="East Coast Super Subs"
+              src="https://fastly.4sqi.net/img/general/600x600/66bCDReuB4wMmyYyMncP3nbB8uHnUMcMyPFzli8A_1Q.jpg"
+            />
+            <p>East Coast Super Subs</p>
+          </li>
+        </ul>
       </div>
     </div>
   );
