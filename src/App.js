58259a0e8b4ef3accad590ba0eae2e5466d3f702
Colin Reeder <63566406+colin-shreeder@users.noreply.github.com>
2020-12-18 12:01:04 -0700

    Revert "Added routes/router"

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import {withRouter} from 'react-router-dom'
-import Header from './Components/Header/Header'
-import routes from './routes'
-
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      {routes}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
