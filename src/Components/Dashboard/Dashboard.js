83376b5c2e019bbe8bec968c7570db86cd3fc03c
Steven Larsen <slarsen0823@gmail.com>
2020-12-28 09:27:14 -0700

    commit

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ const Dashboard = () => {
     <div className="dashboard">
       <div className="dash-header">
         <h3 className="welcome">Welcome to the state of:</h3>
-        <h1 className="state">Your State Here</h1>
+        <h1 className="state">(Insert State Here)</h1>
         <h4>Check out some of these great places to eat</h4>
       </div>
       <div>
