b78370377d89a8b4357b572911f50ded74b7233a
Colin Reeder <colin.c.reeder@gmail.com>
2020-12-29 10:52:21 -0700

    created controller, delete by id request, get by id request

---
+++ server/controller.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    delete: ( req, res, next ) => {
+        const dbInstance = req.app.get('db');
+        const { postid } = req.params;
+      
+        dbInstance.delete( postid )
+          .then( (response) => res.status(200).send(response) )
+          .catch( err => {
+            res.status(500).send({errorMessage: "Oops! Something went wrong. Our engineers have been informed!"});
+            console.log(err)
+          } );
+      },
+
+      getPost: (req,res,next) => {
+        let { postid } = req.params;
+        const db = req.app.get("db");
+      
+        db.get_by_id([postid])
+          .then(response => {
+            res.status(200).send(response);
+          })
+          .catch(err => res.status(500).send(err));
+        }
+}
