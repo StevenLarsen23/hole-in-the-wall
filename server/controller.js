229a6d82a5d33a7a7aa9aba0fd8c9237ad85d6cd
Tanner Christensen <tannerchristensen20@gmail.com>
2020-12-30 16:29:35 -0700

    got map to work

--- server/controller.js
+++ server/controller.js
@@ -2,6 +2,7 @@ module.exports = {
   getAllStates: async (req, res) => {
     const db = req.app.get("db");
     const allStates = await db.get_all_states();
+    console.log('hit')
     res.status(200).send(allStates);
   },
 
