1c1b09a7655c17f9f5b4a6c3925db05141aeb365
Tanner Christensen <tannerchristensen20@gmail.com>
2021-01-04 11:27:16 -0700

    link to dashboard

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import axios from 'axios'
 import { Link } from "react-router-dom"
+import "../Home/Home.css"
 
 class Home extends Component {
     constructor(){
@@ -23,26 +24,16 @@ class Home extends Component {
         console.log(home)
         const displayHome = home.map((e) => {
             return (
-                <div classname='home' key={e.id}>
+                <div className='home' key={e.id}>
+                    <div className='container'>
+                        <Link to= {`/dashboard/${e.id}`} className="Links">
 
-                    <div classname='container'>
+                            <div className='stateName'>{e.state_name}</div>
 
-                        <div classname='stateName'>
+                            <div className='stateImg'> <img src={e.state_img} alt={e.state_name}/> </div>
 
-                            {e.state_name}
-
-                        </div>
-
-                        <div classname='stateImg'>
-
-                            <img
-                            src={e.state_img} alt={e.state_name}
-                            />
-
-                        </div>
-
+                        </Link>
                     </div>
-
                 </div>
             )
         })
