be66bf035c17431a53fc7c809064767fca6616ea
Philip <psain87@hotmail.com>
2021-01-04 11:22:35 -0600

    pushing up all changes

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+import './Form.css';
+import Dropdown from '../Dropdown/Dropdown';
+import states from '../Dropdown/states.json';
 
 const Form = () => {
 
     const [data, setData] = useState();
+    const [value, setValue] = useState(null);
 
     const history = useHistory();
 
-   
+
     const newPost = async () => {
         const { name, img, content } = data
         await axios.post("/api/create", { name, img, content })
@@ -23,25 +27,50 @@ const Form = () => {
     }
 
     return (
-        <form onSubmit={newPost} >
-            <div>
-                <input type='text' placeholder='name of place' onChange={e => handleChange(e)} ></input>
-            </div>
 
-            <div>
-                <input type='text' placeholder='location of place' onChange={e => handleChange(e)} ></input>
-            </div>
+        <div className='div-main'>
+            <h1>Add your own favorite Hole-in-the-Wall!</h1>
 
-            <div>
-                <input type='image' ></input>
+            <form className='form-main' onSubmit={newPost} >
+                <div>
+                    <input className='name-input input'
+                        type='text'
+                        placeholder='name of place'
+                        onChange={e => handleChange(e)} >
+                    </input>
+                </div>
 
-            </div>
-            <div>
-                <input type='text' placeholder='description of new location' onChange={e => handleChange(e)} ></input>
-            </div>
+                <div>
+                    <div >
+                        <Dropdown
+                            states={states}
+                            prompt='Select a state...'
+                            value={value}
+                            onChange={val => setValue(val)}
+                        />
+                    </div>
+                </div>
 
-            <button type='submit'>Add New Spot</button>
-        </form>
+                <div>
+                    <input
+                        className='img-input input'
+                        placeholder='img URL'>
+                    </input>
+
+                </div>
+                <div>
+                    <input
+                        className='descrip-box'
+                        type='text'
+                        placeholder='description'
+                        onChange={e => handleChange(e)} >
+
+                    </input>
+                </div>
+
+                <button type='submit'>Add New Spot</button>
+            </form>
+        </div>
     )
 }
 
